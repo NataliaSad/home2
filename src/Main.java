702c84b7bb7397b043c6d364aca69e5f9b5c229e
ЛЁША <kovnatali@mail.ru>
2022-12-27 02:03:47 +0700

    first commit second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,88 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello world!");}}
+    public static void main(String[] args) {System.out.println("Задачи");
+    var dog = 8.0;
+    System.out.println(dog);
+    var cat = 3.6;
+    System.out.println(cat);
+    var paper = 763789;
+    System.out.println(paper);
+    dog = dog + 4;
+    System.out.println(dog);
+    cat  = cat + 4;
+    System.out.println(cat);
+    paper = paper + 4;
+    System.out.println(paper);
+    dog = dog -= 3.5;
+    System.out.println(dog);
+    cat = cat -=1.6;
+    System.out.println(cat);
+    paper = paper -=7639;
+    System.out.println(paper);
+    var friend = 19;
+    System.out.println(friend);
+    friend =  friend + 2;
+    System.out.println(friend);
+    friend = friend / 7;
+    System.out.println(friend);
+    var frog = 3.5;
+    System.out.println(frog);
+        frog = frog * 10;
+      System.out.println(frog);
+        frog = frog /3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
+        var first = 78.2;
+        System.out.println(first);
+        var second = 82.7;
+        System.out.println(second);
+        var general = first + second;
+        System.out.println("общий вес"  + general);
+        var difference = second - first;
+        System.out.println(difference);
+        var difference1 = second % first;
+        System.out.println(difference1);
+        var employee = 640;
+        var each = 8;
+        var total = employee / each;
+        System.out.println("всего работников в компании" + total);
+        var more = 94;
+        var only = (total + more);
+        System.out.println( "итого в компании работает" + only);
+        var totalHours = only / each;
+        System.out.println( "всего часов работы может быть между сотрудниками" + totalHours);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
